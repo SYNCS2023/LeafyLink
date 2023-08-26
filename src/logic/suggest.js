a2626fea275f5fd0d7e53bc769d782dae69d8b27
Larissa Yip <larissa.yip@student.unsw.edu.au>
2023-08-26 22:57:28 +1000

    add light and harvest time param to suggest algo

--- src/logic/suggest.js
+++ src/logic/suggest.js
@@ -1,9 +1,9 @@
 import plantData from './plantData.json' assert { type: 'json' };
 
-function getClimateData(lattitude, longitude) {
+function getClimateData(latitude, longitude) {
   return fetch(
     'http://climateapi.scottpinkelman.com/api/v1/location/' +
-      lattitude +
+      latitude +
       '/' +
       longitude
   );
@@ -21,18 +21,85 @@ function spaceAvailable(spaceReq, property) {
 }
 
 function distanceHarvestTime(plant, time) {
-    const harvest_time = plant.harvest_time;
+    const harvestTime = plant.harvest_time;
+    let midTime = 1;
 
+    // via exp dist
+
+    // param tuning
     if (time === ">1") {
-        time = 1; 
+        if (harvestTime <= 1) {
+            return 7;
+        }
+
+        midTime = 1;
     } else if (time === "1-3") {
-        time = 2;
+        if (harvestTime > 1 && harvestTime <= 3) {
+            return 7;
+        }
+        
+        midTime = 3;
     } else if (time === "3-6") {
-        time = 5;
+        if (harvestTime > 3 && harvestTime <= 6) {
+            return 7;
+        }
+
+        midTime = 6;
     } else if (time === "6-12") {
-        time = 9;
-    } else {
+        if (harvestTime > 6 && harvestTime <= 12) {
+            return 7;
+        }
+
+        midTime = 12;
+    } else { // 12+
+        if (harvestTime > 12) {
+            return 7;
+        }
         time = 12;
+    }
+
+    return 7 - 4 * Math.log(Math.abs(harvestTime - midTime));
+}
+
+function getSeasonalLightConditions(latitude) {
+    if (latitude <= 40 && latitude >= -40) {
+        return "full";
+    }
+
+    const month = (new Date()).getMonth();
+    let isSummer = false;
+    if (latitude >= 0) { // northern
+        if (month >= 3 && month <= 8) isSummer = true;
+    } else { // southern
+        if (month >= 8 || month <= 2) {
+            isSummer = true;
+        }
+    }
+
+    if (latitude > 40 && latitude <= 90) { // north
+        if (isSummer) {
+            return "full";
+        } else { // winter
+            if (latitude <= 50) {
+                return "full";
+            } else if (latitude <= 60) {
+                return "partial";
+            } else {
+                return "shade";
+            }
+        }
+    } else if (latitude < -40 && latitude >= -90) { // south
+        if (isSummer) { 
+            return "full";
+        } else { // winter
+            if (latitude >= -50) {
+                return "full";
+            } else if (latitude >= -60) {
+                return "partial";
+            } else {
+                return "shade";
+            }
+        }
     }
 }
 
@@ -44,12 +111,14 @@ async function suggestPlants(budget, location, property, time, potted) {
 
     console.log("Ordered suggestions for input of: " + location.latitude + " " + location.longitude + " " + property + " " + time + " " + potted);
 
+    // param tuning
     for (let [key, value] of Object.entries(plantData.data)) {
         if (spaceAvailable(value.space_req, property)) {
             let index = suggestions.push({name: value.name, score: 0}) - 1;
             if (value.kgz_1.includes(kgz_1User)) {
                 suggestions[index].score = suggestions[index].score + 3;
             }
+
             if (potted === "pot") {
                 if (value.pots) {
                     suggestions[index].score = suggestions[index].score + 5;
@@ -59,6 +128,17 @@ async function suggestPlants(budget, location, property, time, potted) {
                     suggestions[index].score = suggestions[index].score + 5;
                 }
             }
+
+            const seasonalLight = getSeasonalLightConditions(location.latitude);
+            if (value.light_level.includes(seasonalLight)) {
+                if (potted === "ground") {
+                    suggestions[index].score = suggestions[index].score + 5;
+                } else { // potted
+                    suggestions[index].score = suggestions[index].score + 2;
+                }
+            }
+
+            suggestions[index].score = suggestions[index].score + distanceHarvestTime(value, time);   
         }
     }
 
@@ -104,5 +184,7 @@ console.log(
       "ground"
     )
   );
+
+// console.log((new Date()).getMonth());
 
 export default suggestPlants;
