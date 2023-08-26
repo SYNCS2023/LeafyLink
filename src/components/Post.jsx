4a06e905c6c20fc71fb02f609d9028b6cfb04c3d
Luke Prior <lukeprior3@gmail.com>
2023-08-26 14:56:25 +1000

    wip

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,7 +5,7 @@ const Post = (props) => {
     <div>
         <div className="card card-compact w-96 bg-base-100 shadow-xl p-0">
         <figure className="h-60">
-            <img className="object-cover hover:scale-110 transition duration-500 cursor-pointer" src={ props.image } alt={ props.type } />
+            <img className="hover:scale-110 transition duration-500 cursor-pointer" src={ props.image } alt={ props.type } />
         </figure>
         <div className="card-body">
             <h2 className="card-title uppercase text-primary text-2xl mb-0">{ props.name }</h2>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <div className="grid grid-cols-1 gap-4 content-center justify-items-center">
       <Post name="Ryan" type="Cherry Tomato" image="https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQI3JN84mIeTu2LuFuMIjJZyOR8bF8SrckvuaerXftethNxn8sKNptrLAKgliu5lagHMEqSBrhZz_IO9jw" />
       <Post name="Luke" type="Carrot" image="https://www.economist.com/cdn-cgi/image/width=1424,quality=80,format=auto/sites/default/files/20180929_BLP506.jpg" />
-      <Post name="Ryan" type="Cherry Tomato" image="https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQI3JN84mIeTu2LuFuMIjJZyOR8bF8SrckvuaerXftethNxn8sKNptrLAKgliu5lagHMEqSBrhZz_IO9jw" />
+      <Post name="OzHarvest" type="OzHarvest" image="https://scontent.fcbr1-1.fna.fbcdn.net/v/t39.30808-6/347246174_220047104191485_3532220201324838855_n.png?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=pLpSfU0PMokAX-dhcNF&_nc_ht=scontent.fcbr1-1.fna&oh=00_AfDvSAFhGri9XoSpOvSXz6FqBulCsxJcboXSczgZ9RIogw&oe=64EDD610" />
       <Post name="Ryan" type="Cherry Tomato" image="https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQI3JN84mIeTu2LuFuMIjJZyOR8bF8SrckvuaerXftethNxn8sKNptrLAKgliu5lagHMEqSBrhZz_IO9jw" />
       <Post name="Ryan" type="Cherry Tomato" image="https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQI3JN84mIeTu2LuFuMIjJZyOR8bF8SrckvuaerXftethNxn8sKNptrLAKgliu5lagHMEqSBrhZz_IO9jw" />
       <Post name="Ryan" type="Cherry Tomato" image="https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQI3JN84mIeTu2LuFuMIjJZyOR8bF8SrckvuaerXftethNxn8sKNptrLAKgliu5lagHMEqSBrhZz_IO9jw" />
