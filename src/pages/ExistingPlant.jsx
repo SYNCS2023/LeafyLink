4bf3c4d0afa4d31cc8e57b82436febf27d661831
Haowen <jqxczz@gmail.com>
2023-08-26 14:56:09 +1000

    add select to existing plant

--- src/pages/ExistingPlant.jsx
+++ src/pages/ExistingPlant.jsx
@@ -3,7 +3,16 @@ const ExistingPlant = () => {
   return (
     <div>
       <p>existing plant</p>
-      <input accept="image/*" id="icon-button-file" type="file" capture="environment" />
+      <input type="file" className="file-input w-full max-w-xs" accept="image/*" capture="environment"/>
+      <select className='select select-bordered w-full max-w-xs'>
+        <option disabled selected>
+          Select your plant...
+        </option>
+        <option>Plant a</option>
+        <option>Plant b</option>
+        <option>Plant c</option>
+        <option>Plant d</option>
+      </select>
     </div>
   );
 };
