d56873ec561dc4a0db54a140ef9f2f07afad3c59
wanning <z5371162@unsw.edu.au>
2023-08-26 14:59:46 +1000

    context wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,23 @@ import Quiz from './pages/Quiz';
 import NotFound from './pages/NotFound';
 import SuggestPlant from './pages/SuggestPlant';
 import ExistingPlant from './pages/ExistingPlant';
+import AppContextProvider from './contexts/appContext';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/garden' element={<Garden />} />
-          <Route path='/quiz' element={<Quiz />} />
-          <Route path='/new' element={<SuggestPlant />} />
-          <Route path='/existing' element={<ExistingPlant />} />
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <AppContextProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/garden' element={<Garden />} />
+            <Route path='/quiz' element={<Quiz />} />
+            <Route path='/new' element={<SuggestPlant />} />
+            <Route path='/existing' element={<ExistingPlant />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </AppContextProvider>
     </>
   );
 }
