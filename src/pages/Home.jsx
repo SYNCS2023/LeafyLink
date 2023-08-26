96ed2addef8992751e3ef9b11aac98f24434fb1f
Luke Prior <lukeprior3@gmail.com>
2023-08-26 15:04:12 +1000

    main update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,14 +4,14 @@ import Plant from '../components/Plant';
 const Home = () => {
   return <div>
     <div className="grid grid-cols-1 gap-4 content-center justify-items-center">
-      <Post name="Ryan" type="Cherry Tomato" image="https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQI3JN84mIeTu2LuFuMIjJZyOR8bF8SrckvuaerXftethNxn8sKNptrLAKgliu5lagHMEqSBrhZz_IO9jw" />
-      <Post name="Luke" type="Carrot" image="https://www.economist.com/cdn-cgi/image/width=1424,quality=80,format=auto/sites/default/files/20180929_BLP506.jpg" />
-      <Post name="OzHarvest" type="OzHarvest" image="https://scontent.fcbr1-1.fna.fbcdn.net/v/t39.30808-6/347246174_220047104191485_3532220201324838855_n.png?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=pLpSfU0PMokAX-dhcNF&_nc_ht=scontent.fcbr1-1.fna&oh=00_AfDvSAFhGri9XoSpOvSXz6FqBulCsxJcboXSczgZ9RIogw&oe=64EDD610" />
-      <Post name="Ryan" type="Cherry Tomato" image="https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQI3JN84mIeTu2LuFuMIjJZyOR8bF8SrckvuaerXftethNxn8sKNptrLAKgliu5lagHMEqSBrhZz_IO9jw" />
-      <Post name="Ryan" type="Cherry Tomato" image="https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQI3JN84mIeTu2LuFuMIjJZyOR8bF8SrckvuaerXftethNxn8sKNptrLAKgliu5lagHMEqSBrhZz_IO9jw" />
-      <Post name="Ryan" type="Cherry Tomato" image="https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQI3JN84mIeTu2LuFuMIjJZyOR8bF8SrckvuaerXftethNxn8sKNptrLAKgliu5lagHMEqSBrhZz_IO9jw" />
-      <Post name="Ryan" type="Cherry Tomato" image="https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQI3JN84mIeTu2LuFuMIjJZyOR8bF8SrckvuaerXftethNxn8sKNptrLAKgliu5lagHMEqSBrhZz_IO9jw" />
-      <Post name="Ryan" type="Cherry Tomato" image="https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQI3JN84mIeTu2LuFuMIjJZyOR8bF8SrckvuaerXftethNxn8sKNptrLAKgliu5lagHMEqSBrhZz_IO9jw" />
+      <Plant name="Ryan" type="Cherry Tomato" img="/images/ryan.jpeg" />
+      <Plant name="Luke" type="Potato" img="/images/luke.png" />
+      <Plant name="OzHarvest" type="OzHarvest" img="https://scontent.fcbr1-1.fna.fbcdn.net/v/t39.30808-6/347246174_220047104191485_3532220201324838855_n.png?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=pLpSfU0PMokAX-dhcNF&_nc_ht=scontent.fcbr1-1.fna&oh=00_AfDvSAFhGri9XoSpOvSXz6FqBulCsxJcboXSczgZ9RIogw&oe=64EDD610" />
+      <Plant name="Lari" type="Cucumber" img="/images/lari.png" />
+      <Plant name="Ryan" type="Cherry Tomato" img="/images/ryan.jpeg" />
+      <Plant name="Ryan" type="Cherry Tomato" img="/images/ryan.jpeg" />
+      <Plant name="Ryan" type="Cherry Tomato" img="/images/ryan.jpeg" />
+      <Plant name="Ryan" type="Cherry Tomato" img="/images/ryan.jpeg" />
     </div>
   </div>;
 };
