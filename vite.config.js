ce5bee9407dfb74a92f0353b37ec6cad48d3b1a5
Luke Prior <22492406+Uskompuf@users.noreply.github.com>
2023-08-27 00:05:20 +1000

    fix vite config?

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
+export default defineConfig(({ command, mode }) => {
+  if (command === 'build') {
+    return {
+      plugins: [react()],
+      base: '/LeafyLink/'
+    }
+  } else {
+    return {
+      plugins: [react()]
+    }
+  }
 })
