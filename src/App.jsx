26aac5b414ac836b005915b201b93e6d3449099a
name <email>
2023-08-26 13:46:25 +1000

    routes working

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Garden from './pages/Garden';
 import Home from './pages/Home';
 import Quiz from './pages/Quiz';
@@ -8,12 +8,14 @@ import NotFound from './pages/NotFound';
 function App() {
   return (
     <>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/garden' element={<Garden />} />
-        <Route path='/quiz' element={<Quiz />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/garden' element={<Garden />} />
+          <Route path='/quiz' element={<Quiz />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
