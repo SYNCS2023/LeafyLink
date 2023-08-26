e76443d244aac4e039682978976b89fec9f0b751
wanning <z5371162@unsw.edu.au>
2023-08-27 07:53:00 +1000

    green theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,34 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: '#4406CB',
+        primary: '#047857',
       },
     },
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#047857',
+
+          secondary: '#65a30d',
+
+          accent: '#0f766e',
+
+          neutral: '#2b3440',
+
+          'base-100': '#ffffff',
+
+          info: '#3abff8',
+
+          success: '#36d399',
+
+          warning: '#fbbd23',
+
+          error: '#f87272',
+        },
+      },
+    ],
   },
   plugins: [require('daisyui'), require('@tailwindcss/typography')],
 };
