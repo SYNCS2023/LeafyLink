ab64fcfc72e751bf238263a50f3f9d98fa57da93
RYNO8 <35283610+RYNO8@users.noreply.github.com>
2023-08-26 16:26:19 +1000

    troubleshoot random & menubar padding

--- src/pages/Garden.jsx
+++ src/pages/Garden.jsx
@@ -3,28 +3,29 @@ import Plant from '../components/Plant';
 import Menubar from '../components/Menubar';
 
 const Garden = () => {
-    return (
-        <div>
-            <div className="grid grid-cols-1 md:grid-cols-2 justify-center">
-                <div className="flex flex-initial p-10 justify-center">
-                    <Plant name="Lari" type="Cucumber" likes="133" img="/images/lari.png" age={0} owned={true} />
-                </div>
-                <div className="flex flex-initial p-10 justify-center">
-                    <Plant name="Ryan" type="Tomato" likes="133" img="/images/ryan.jpeg" age={1} owned={true} />
-                </div>
-                <div className="flex flex-initial p-10 justify-center">
-                    <Plant name="Luke" type="Potato" likes="133" img="/images/luke.png" age={2} owned={true} />
-                </div>
-                <div className="flex flex-initial p-10 justify-center">
-                    <Plant name="Wanning" type="Artichoke" likes="113" img="/images/wanning.jpg" age={3} owned={true} />
-                </div>
-                <div className="flex flex-initial p-10 justify-center">
-                    <Plant name="Haowen" type="Mandarin" likes="113" img="/images/haowen.webp" age={4} owned={true} />
-                </div>
-            </div>
-            <Menubar active="garden" />
+  return (
+    <div>
+      <h1 className='text-lg'>Your Garden</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 justify-center">
+        <div className="flex flex-initial justify-center">
+          <Plant name="Lari" type="Cucumber" likes="133" img="/images/lari.png" age={0} owned={true} />
         </div>
-    )
+        <div className="flex flex-initial justify-center">
+          <Plant name="Ryan" type="Tomato" likes="133" img="/images/ryan.jpeg" age={1} owned={true} />
+        </div>
+        <div className="flex flex-initial justify-center">
+          <Plant name="Luke" type="Potato" likes="133" img="/images/luke.png" age={2} owned={true} />
+        // </div>
+        <div className="flex flex-initial justify-center">
+          <Plant name="Wanning" type="Artichoke" likes="113" img="/images/wanning.jpg" age={3} owned={true} />
+        // </div>
+        <div className="flex flex-initial justify-center">
+          <Plant name="Haowen" type="Mandarin" likes="113" img="/images/haowen.webp" age={4} owned={true} />
+        </div>
+      </div>
+      <Menubar active="garden" />
+    </div>
+  )
 };
 
 export default Garden;
