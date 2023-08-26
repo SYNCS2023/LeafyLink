072232db49e2d2534c0060e5210ebdce6d97b418
RYNO8 <35283610+RYNO8@users.noreply.github.com>
2023-08-26 14:55:15 +1000

    garden mockup and plant component

--- src/pages/Garden.jsx
+++ src/pages/Garden.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import Plant from '../components/Plant';
 
 const Garden = () => {
-  return <div>Garden</div>;
+    return (
+        // <div className="card bg-base-200 w-80">
+        //   <div className="card-body">
+        //     <div className="card w-96 bg-base-100 shadow-xl">
+        //       <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+        //       <div className="card-body">
+        //         <h2 className="card-title">Shoes!</h2>
+        //         <p>If a dog chews shoes whose shoes does he choose?</p>
+        //         <div className="card-actions justify-end">
+        //           <button className="btn btn-primary">Buy Now</button>
+        //         </div>
+        //       </div>
+        //     </div>
+        //   </div>
+        // </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 justify-center">
+            <div className="flex flex-initial p-10 justify-center">
+                <Plant name="Lari" type="Cucumber" likes="133" img="/images/lari.png" todo={true} owned={true} />
+            </div>
+            <div className="flex flex-initial p-10 justify-center">
+                <Plant name="Ryan" type="Tomato" likes="133" img="/images/ryan.jpeg" todo={false} owned={true} />
+            </div>
+            <div className="flex flex-initial p-10 justify-center">
+                <Plant name="Luke" type="Potato" likes="133" img="/images/luke.png" todo={false} owned={true} />
+            </div>
+            {/* <div className="flex flex-initial p-10 justify-center">
+                <Plant />
+            </div>
+            <div className="flex flex-initial p-10 justify-center">
+                <Plant />
+            </div> */}
+        </div>
+    )
 };
 
 export default Garden;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import Post from '../components/Post';
+import Plant from '../components/Plant';
 
 const Home = () => {
   return <div>
     <div className="grid grid-cols-1 gap-4 content-center justify-items-center">
-      <Post name="Ryan" type="Cherry Tomato" image="https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQI3JN84mIeTu2LuFuMIjJZyOR8bF8SrckvuaerXftethNxn8sKNptrLAKgliu5lagHMEqSBrhZz_IO9jw" />
-      <Post name="Luke" type="Carrot" image="https://www.economist.com/cdn-cgi/image/width=1424,quality=80,format=auto/sites/default/files/20180929_BLP506.jpg" />
-      <Post name="Ryan" type="Cherry Tomato" image="https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQI3JN84mIeTu2LuFuMIjJZyOR8bF8SrckvuaerXftethNxn8sKNptrLAKgliu5lagHMEqSBrhZz_IO9jw" />
-      <Post name="Ryan" type="Cherry Tomato" image="https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQI3JN84mIeTu2LuFuMIjJZyOR8bF8SrckvuaerXftethNxn8sKNptrLAKgliu5lagHMEqSBrhZz_IO9jw" />
-      <Post name="Ryan" type="Cherry Tomato" image="https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQI3JN84mIeTu2LuFuMIjJZyOR8bF8SrckvuaerXftethNxn8sKNptrLAKgliu5lagHMEqSBrhZz_IO9jw" />
-      <Post name="Ryan" type="Cherry Tomato" image="https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQI3JN84mIeTu2LuFuMIjJZyOR8bF8SrckvuaerXftethNxn8sKNptrLAKgliu5lagHMEqSBrhZz_IO9jw" />
-      <Post name="Ryan" type="Cherry Tomato" image="https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQI3JN84mIeTu2LuFuMIjJZyOR8bF8SrckvuaerXftethNxn8sKNptrLAKgliu5lagHMEqSBrhZz_IO9jw" />
-      <Post name="Ryan" type="Cherry Tomato" image="https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQI3JN84mIeTu2LuFuMIjJZyOR8bF8SrckvuaerXftethNxn8sKNptrLAKgliu5lagHMEqSBrhZz_IO9jw" />
+      <Plant name="Ryan" type="Cherry Tomato" img="https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQI3JN84mIeTu2LuFuMIjJZyOR8bF8SrckvuaerXftethNxn8sKNptrLAKgliu5lagHMEqSBrhZz_IO9jw" />
+      <Plant name="Luke" type="Carrot" img="https://www.economist.com/cdn-cgi/image/width=1424,quality=80,format=auto/sites/default/files/20180929_BLP506.jpg" />
+      <Plant name="Ryan" type="Cherry Tomato" img="https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQI3JN84mIeTu2LuFuMIjJZyOR8bF8SrckvuaerXftethNxn8sKNptrLAKgliu5lagHMEqSBrhZz_IO9jw" />
+      <Plant name="Ryan" type="Cherry Tomato" img="https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQI3JN84mIeTu2LuFuMIjJZyOR8bF8SrckvuaerXftethNxn8sKNptrLAKgliu5lagHMEqSBrhZz_IO9jw" />
+      <Plant name="Ryan" type="Cherry Tomato" img="https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQI3JN84mIeTu2LuFuMIjJZyOR8bF8SrckvuaerXftethNxn8sKNptrLAKgliu5lagHMEqSBrhZz_IO9jw" />
+      <Plant name="Ryan" type="Cherry Tomato" img="https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQI3JN84mIeTu2LuFuMIjJZyOR8bF8SrckvuaerXftethNxn8sKNptrLAKgliu5lagHMEqSBrhZz_IO9jw" />
+      <Plant name="Ryan" type="Cherry Tomato" img="https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQI3JN84mIeTu2LuFuMIjJZyOR8bF8SrckvuaerXftethNxn8sKNptrLAKgliu5lagHMEqSBrhZz_IO9jw" />
+      <Plant name="Ryan" type="Cherry Tomato" img="https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQI3JN84mIeTu2LuFuMIjJZyOR8bF8SrckvuaerXftethNxn8sKNptrLAKgliu5lagHMEqSBrhZz_IO9jw" />
 
     </div>
   </div>;
